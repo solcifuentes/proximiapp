b320f661b14ad1ce7321de359e9ea5ba8bfd7185
Sol Cifuentes <msolcifuentes@gmail.com>
2023-11-16 17:53:57 +0100

    mapping json data

--- src/presentation/components/InputSearchSelector.tsx
+++ src/presentation/components/InputSearchSelector.tsx
@@ -1,21 +1,27 @@
-import { IconChevronDown } from '@tabler/icons-react'
-import classnames from 'classnames'
-import { useEffect } from 'react'
+import { IconChevronDown } from "@tabler/icons-react";
+import classnames from "classnames";
+import { useEffect } from "react";
 
 type Props = {
-  className?: string,
-  selectClassName?: string,
-  name: string,
-  options: { value: any, label: string }[],
-  onChange?: (value: string) => void,
-}
+  className?: string;
+  selectClassName?: string;
+  name: string;
+  options: { value: any; label: string }[];
+  onChange?: (value: string) => void;
+};
 
-export const InputSearchSelector = ({ className, selectClassName, name, options, onChange }: Props) => {
-  const classes = classnames('relative flex min-w-[100px]', className)
+export const InputSearchSelector = ({
+  className,
+  selectClassName,
+  name,
+  options,
+  onChange,
+}: Props) => {
+  const classes = classnames("relative flex min-w-[100px]", className);
 
   useEffect(() => {
-    onChange?.(options[0].value)
-  }, [])
+    onChange?.(options[0].value);
+  }, []);
 
   return (
     <div className={classes}>
@@ -29,14 +35,18 @@ export const InputSearchSelector = ({ className, selectClassName, name, options,
             font-light text-white outline-0 backdrop-blur-md ${selectClassName}
           `}
           style={{
-            WebkitAppearance: 'none',
+            WebkitAppearance: "none",
           }}
         >
-          {options.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
+          {options.map(({ value, label }) => (
+            <option key={value} value={value}>
+              {label}
+            </option>
+          ))}
         </select>
 
-        <IconChevronDown className="absolute right-1 top-2 h-[1.1rem] text-gray-300"/>
+        <IconChevronDown className="absolute right-1 top-2 h-[1.1rem] text-gray-300" />
       </div>
     </div>
-  )
-}
+  );
+};
--- src/presentation/pages/dashboard/Dashboard.tsx
+++ src/presentation/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import { InputSearchSelector } from "@/presentation/components/InputSearchSelector";
+import comercialTypes from "@/resources/repositories/district/json/comercialTypes.json";
+
 export const Dashboard = () => {
+  const [selectedType, setSelectedType] = useState<string | undefined>(
+    undefined
+  );
+
+  const handleTypeChange = (value: string) => {
+    const selectedOption = comercialTypes.find(
+      (option) => option.value === Number(value)
+    );
+    setSelectedType(selectedOption ? selectedOption.label : undefined);
+  };
+
   return (
     <main>
-      Dashboard
+      <InputSearchSelector
+        name="type"
+        options={comercialTypes}
+        onChange={handleTypeChange}
+      />
+      {selectedType && <p>Chosen business: {selectedType}</p>}
     </main>
-  )
-}
+  );
+};
